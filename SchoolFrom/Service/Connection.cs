128e532ae4d473dd3f6ac1b19031a64eab5833d3
Dan_Ostap <84470259+DanOstap@users.noreply.github.com>
2023-11-01 21:32:10 +0100

    Update Connection.cs

--- SchoolFrom/Service/Connection.cs
+++ SchoolFrom/Service/Connection.cs
@@ -2,7 +2,7 @@
 {
     public class Connection
     {
-        public string DataBase_Url { get; set; } = "mongodb+srv://dan_ostap:Frost310@cluster0.1ecvuza.mongodb.net/?retryWrites=true&w=majority";
+        public string DataBase_Url { get; set; } = "";
         public string DataBase_Name { get; set; } = "School";
         public string DataBase_Collection { get; set; } = "Candidates";
     }
